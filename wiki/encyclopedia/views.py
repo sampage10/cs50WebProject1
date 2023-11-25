2182018d329ea2facf691cc0698518ecab5eb8d3
sampage10 <134566800+sampage10@users.noreply.github.com>
2023-11-25 14:29:41 -0500

    hyperlinks and search funtion

--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -27,4 +27,24 @@ def entry(request, title):
         return render(request, "encyclopedia/entry.html", {
             "title": title,
             "content": html_content
-        }) 
+        })
+
+def search(request):
+    if request.method == "POST":
+        entry_search = request.POST['q']
+        html_content = convert_md_to_html(entry_search)
+        if html_content is not None:
+            return render(request, "encyclopedia/entry.html", {
+            "title": entry_search,
+            "content": html_content
+        })
+        else:
+            allEntries = util.list_entries()
+            recommendation = []
+            for entry in allEntries:
+                if entry_search.lower() in entry.lower():
+                    recommendation.append(entry)
+                render(request, "encyclopedia/search.html", {
+                    "recommendation": recommendation
+                })
+            
